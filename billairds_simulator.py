b189ac0773a85638ce137f13b2f284f4e4b12824
viankahmercado <angelviankahmercado@gmail.com>
2024-02-09 06:59:21 +0800

    program complete

--- billairds_simulator.py
+++ billairds_simulator.py
@@ -12,28 +12,28 @@ import math
 
 pygame.init()
 
-# screen height
+#screen height
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 678
 BOTTOM_PANEL = 50
 
-# game window
+#game window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT + BOTTOM_PANEL))
 pygame.display.set_caption("Billards Simulator")
 
-# pymunk space
+#pymunk space
 space = pymunk.Space()
 static_body = space.static_body
 draw_options = pymunk.pygame_util.DrawOptions(screen)
 
-# clock
+#clock
 clock = pygame.time.Clock()
 FPS = 120
 
 #game variables
 lives = 3
-diameter = 36
-pocket_diameter = 66
+dia = 36
+pocket_dia = 66
 force = 0
 max_force = 10000
 force_direction = 1
@@ -43,7 +43,7 @@ taking_shot = True
 powering_up = False
 potted_balls = []
 
-# colors
+#colors
 background = (50, 50, 50)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
@@ -52,7 +52,7 @@ WHITE = (255, 255, 255)
 font = pygame.font.SysFont("Lato", 30)
 large_font = pygame.font.SysFont("Lato", 60)
 
-# images path
+#images path
 cue_image = pygame.image.load("images/cue.png").convert_alpha()
 table_image = pygame.image.load("images/table.png").convert_alpha()
 ball_images = []
@@ -60,43 +60,42 @@ for i in range(1, 17):
   ball_image = pygame.image.load(f"images/ball_{i}.png").convert_alpha()
   ball_images.append(ball_image)
 
-# function for displaying text onto the screen
+#function for displaying text onto the screen
 def draw_text(text, font, text_col, x, y):
   img = font.render(text, True, text_col)
   screen.blit(img, (x, y))
 
-# function for creating balls
+#function for creating balls
 def create_ball(radius, pos):
   body = pymunk.Body()
   body.position = pos
   shape = pymunk.Circle(body, radius)
   shape.mass = 5
   shape.elasticity = 0.8
-  # to add friction use pivot joint
+  #to add friction use pivot joint
   pivot = pymunk.PivotJoint(static_body, body, (0, 0), (0, 0))
   pivot.max_bias = 0 # disable joint correction
   pivot.max_force = 1000 # emulate linear friction
 
   space.add(body, shape, pivot)
-  return shape 
+  return shape
 
-# set up position of the balls
+#set up position of the balls
 balls = []
 rows = 5
-# potting balls
+#potting balls
 for col in range(5):
   for row in range(rows):
-    pos = (250 + (col * (diameter + 1)), 267 + (row * (diameter + 1)) + (col * diameter / 2))
-    new_ball = create_ball(diameter / 2, pos)
+    pos = (250 + (col * (dia + 1)), 267 + (row * (dia + 1)) + (col * dia / 2))
+    new_ball = create_ball(dia / 2, pos)
     balls.append(new_ball)
   rows -= 1
+#cue ball
+pos = (888, SCREEN_HEIGHT / 2)
+cue_ball = create_ball(dia / 2, pos)
+balls.append(cue_ball)
 
-  
-new_ball = create_ball(25, (300, 100))
-
-cue_ball = create_ball(25, (300, 310))
-
-# coordinates of the six pockets on table
+#coordinates of the six pockets on table
 pockets = [
   (55, 63),
   (592, 48),
@@ -106,7 +105,7 @@ pockets = [
   (1134, 616)
 ]
 
-# corner coordinates of each cushions
+#corner coordinates of each cushions
 cushions = [
   [(88, 56), (109, 77), (555, 77), (564, 56)],
   [(621, 56), (630, 77), (1081, 77), (1102, 56)],
@@ -145,34 +144,34 @@ class Cue():
     surface.blit(self.image,
       (self.rect.centerx - self.image.get_width() / 2,
       self.rect.centery - self.image.get_height() / 2)
-)
+     )
 
 cue = Cue(balls[-1].body.position)
 
-# power bar to indicate how powerful the cue ball will be hit 
+#power bar to indicate how powerful the cue ball will be hit 
 power_bar = pygame.Surface((10, 20))
 power_bar.fill(RED)
 
-# create game loop
+#create game loop
 run = True
 while run:
-  
+
   clock.tick(FPS)
   space.step(1 / FPS)
-  
-  # color fill background
+
+  #color fill background
   screen.fill(background)
-  
+
   #draw pool table
   screen.blit(table_image, (0, 0))
 
-#check if any balls have been potted
+  #check if any balls have been potted
   for i, ball in enumerate(balls):
     for pocket in pockets:
       ball_x_dist = abs(ball.body.position[0] - pocket[0])
       ball_y_dist = abs(ball.body.position[1] - pocket[1])
       ball_dist = math.sqrt((ball_x_dist ** 2) + (ball_y_dist ** 2))
-      if ball_dist <= pocket_diameter / 2:
+      if ball_dist <= pocket_dia / 2:
         #check if the potted ball was the cue ball
         if i == len(balls) - 1:
           lives -= 1
@@ -198,20 +197,20 @@ while run:
   #draw pool cue
   if taking_shot == True and game_running == True:
     if cue_ball_potted == True:
-      #reposition the cue ball
+      #reposition cue ball
       balls[-1].body.position = (888, SCREEN_HEIGHT / 2)
       cue_ball_potted = False
     #calculate pool cue angle
     mouse_pos = pygame.mouse.get_pos()
     cue.rect.center = balls[-1].body.position
     x_dist = balls[-1].body.position[0] - mouse_pos[0]
-    y_dist = -(balls[-1].body.position[1] - mouse_pos[1]) 
+    y_dist = -(balls[-1].body.position[1] - mouse_pos[1]) # -ve because pygame y coordinates increase down the screen
     cue_angle = math.degrees(math.atan2(y_dist, x_dist))
     cue.update(cue_angle)
     cue.draw(screen)
 
-# power up pool cue
-if powering_up == True and game_running == True:
+  #power up pool cue
+  if powering_up == True and game_running == True:
     force += 100 * force_direction
     if force >= max_force or force <= 0:
       force_direction *= -1
@@ -220,42 +219,42 @@ if powering_up == True and game_running == True:
       screen.blit(power_bar,
        (balls[-1].body.position[0] - 30 + (b * 15),
         balls[-1].body.position[1] + 30))
-elif powering_up == False and taking_shot == True:
+  elif powering_up == False and taking_shot == True:
     x_impulse = math.cos(math.radians(cue_angle))
     y_impulse = math.sin(math.radians(cue_angle))
-    balls[-1].body.apply_impulse_at_local_point((force * -x_impulse, force * y_impulse))
+    balls[-1].body.apply_impulse_at_local_point((force * -x_impulse, force * y_impulse), (0, 0))
     force = 0
     force_direction = 1
 
-# draw bottom panel
-pygame.draw.rect(screen, background, (0, SCREEN_HEIGHT, SCREEN_WIDTH, BOTTOM_PANEL))
-draw_text("LIVES: " + str(lives), font, WHITE, SCREEN_WIDTH - 200, SCREEN_HEIGHT + 10)
+  #draw bottom panel
+  pygame.draw.rect(screen, background, (0, SCREEN_HEIGHT, SCREEN_WIDTH, BOTTOM_PANEL))
+  draw_text("LIVES: " + str(lives), font, WHITE, SCREEN_WIDTH - 200, SCREEN_HEIGHT + 10)
 
-# display potted balls in bottom panel
-for i, ball in enumerate(potted_balls):
-  screen.blit(ball, (10 + (i * 50), SCREEN_HEIGHT + 10))
+  #display potted balls in bottom panel
+  for i, ball in enumerate(potted_balls):
+    screen.blit(ball, (10 + (i * 50), SCREEN_HEIGHT + 10))
 
-# check for game over
-if lives <= 0:
-  draw_text("GAME OVER", large_font, WHITE, SCREEN_WIDTH / 2 - 160, SCREEN_HEIGHT / 2 - 100)
-  game_running = False
+  #check for game over
+  if lives <= 0:
+    draw_text("GAME OVER", large_font, WHITE, SCREEN_WIDTH / 2 - 160, SCREEN_HEIGHT / 2 - 100)
+    game_running = False
 
-# check if all balls are potted
-if len(balls) == 1:
-  draw_text("YOU WON!", large_font, WHITE, SCREEN_WIDTH / 2 - 160, SCREEN_HEIGHT / 2 - 100)
-  game_running = False
+  # check if all balls are potted
+  if len(balls) == 1:
+    draw_text("YOU WIN!", large_font, WHITE, SCREEN_WIDTH / 2 - 160, SCREEN_HEIGHT / 2 - 100)
+    game_running = False
 
-# events handler
-for event in pygame.event.get():
-  if event.type == pygame.MOUSEBUTTONDOWN and taking_shot == True:
+  # events handler
+  for event in pygame.event.get():
+    if event.type == pygame.MOUSEBUTTONDOWN and taking_shot == True:
       powering_up = True
-  if event.type == pygame.MOUSEBUTTONUP and taking_shot == True:
+    if event.type == pygame.MOUSEBUTTONUP and taking_shot == True:
       powering_up = False
-  if event.type == pygame.QUIT:
+    if event.type == pygame.QUIT:
       run = False
-      
-space.debug_draw(draw_options)
-pygame.display.update()
+        
+  #space.debug_draw(draw_options)
+  pygame.display.update()
       
 pygame.quit()
 

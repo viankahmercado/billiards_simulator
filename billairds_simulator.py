595764de9aa3d7d49d6b7667d7fa5fcc96635c60
viankahmercado <angelviankahmercado@gmail.com>
2024-02-09 02:45:10 +0800

    balls position

--- billairds_simulator.py
+++ billairds_simulator.py
@@ -28,6 +28,9 @@ draw_options = pymunk.pygame_util.DrawOptions(screen)
 clock = pygame.time.Clock()
 FPS = 120
 
+#game variables
+diameter = 36
+
 # colors
 background = (50, 50, 50)
 
@@ -40,6 +43,7 @@ def create_ball(radius, pos):
   body.position = pos
   shape = pymunk.Circle(body, radius)
   shape.mass = 5
+  shape.elasticity = 0.8
   # to add friction use pivot joint
   pivot = pymunk.PivotJoint(static_body, body, (0, 0), (0, 0))
   pivot.max_bias = 0 # disable joint correction
@@ -48,6 +52,17 @@ def create_ball(radius, pos):
   space.add(body, shape, pivot)
   return shape 
 
+# set up position of the balls
+balls = []
+rows = 5
+# potting balls
+for column in range(5):
+  for row in range(rows):
+    position = (250 + (column * (diameter + 1)), 267 + (row *(diameter + 1)) + (column * diameter / 2))
+    new_ball = create_ball(diameter / 2, position)
+    balls.append(new_ball)
+  rows -= 1
+  
 new_ball = create_ball(25, (300, 100))
 
 cue_ball = create_ball(25, (300, 310))
@@ -67,7 +82,8 @@ def create_cushion(poly_dims):
   body = pymunk.Body(body_type = pymunk.Body.STATIC)
   body.position = ((0, 0))
   shape = pymunk.Poly(body, poly_dims)
-
+  shape.elasticity = 0.8
+  
   space.add(body, shape)
 
 for c in cushions:

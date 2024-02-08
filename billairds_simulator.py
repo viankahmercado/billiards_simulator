4f4953d3cb90ab6dc5034b750f838498b76be62e
viankahmercado <angelviankahmercado@gmail.com>
2024-02-09 00:24:20 +0800

    game loop done

--- billairds_simulator.py
+++ billairds_simulator.py
@@ -13,6 +13,19 @@ pygame.init()
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 678
 
-#game window
+# game window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Billards Simulator")
+
+
+# create game loop
+run = True
+while run:
+  
+  # quit event handler
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+      
+pygame.quit()
+

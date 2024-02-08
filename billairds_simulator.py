56548a5274509362524277e8ec3a28f8b941384d
viankahmercado <angelviankahmercado@gmail.com>
2024-02-09 01:49:42 +0800

    adding friction to the ball

--- billairds_simulator.py
+++ billairds_simulator.py
@@ -21,7 +21,7 @@ pygame.display.set_caption("Billards Simulator")
 
 # pymunk space
 space = pymunk.Space()
-space.gravity = (0, 5000)
+static_body = space.static_body
 draw_options = pymunk.pygame_util.DrawOptions(screen)
 
 # clock
@@ -37,8 +37,12 @@ def create_ball(radius, pos):
   body.position = pos
   shape = pymunk.Circle(body, radius)
   shape.mass = 5
+  # to add friction use pivot joint
+  pivot = pymunk.PivotJoint(static_body, body, (0, 0), (0, 0))
+  pivot.max_bias = 0 # disable joint correction
+  pivot.max_force = 1000 # emulate linear friction
 
-  space.add(body, shape)
+  space.add(body, shape, pivot)
   return shape 
 
 new_ball = create_ball(25, (300, 100))
